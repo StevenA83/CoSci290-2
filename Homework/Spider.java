30d0bd05023a03ddbbe806c251fec28b83b9708c
Huoyu <fr440305@gmail.com>
2018-03-08 15:26:39 -0800

    Update Spider.java

--- Homework/Spider.java
+++ Homework/Spider.java
@@ -2,14 +2,49 @@
 // Manager: ___________ (Could you please write your name here?)
 
 public class Spider {
-  private health;
-  private attack;
-  private defense;
+  
+  private int health;
+  private int attack;
+  private int defense;
+  
   public Spider () {
     this.health = 15;
     this.attack = 5;
     this.defense = 5;
   }
+  
+  public int getHealth () {
+    return this.health;
+  }
+  
+  public int getAttack () {
+    return this.attack;
+  }
+  
+  public int getDefense () {
+    return this.defense;
+  }
+  
+  public void setHealth (int h) {
+    this.health = h;
+  }
+  
+  public void setAttack (int a) {
+    this.attack = a;
+  }
+  
+  public void setDefense (int d) {
+    this.defense = d;
+  }
+  
+  public String toString() {
+    return
+      "I am a spider. My informations:\n" +
+      "\t health " + this.health +
+      "\t attack " + this.attack + 
+      "\t defense " + this.defense;
+  }
+  
   public hurtAlien (Alien a) {
     // todo
   }
