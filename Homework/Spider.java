5a2d972b2fc89add573da4283fbc37e0ff5ac2fa
Huoyu <fr440305@gmail.com>
2018-03-08 14:23:48 -0800

    Create Spider.java

---
+++ Homework/Spider.java
@@ -0,0 +1,13 @@
+public class Spider {
+  private health;
+  private attack;
+  private defense;
+  public Spider () {
+    this.health = 15;
+    this.attack = 5;
+    this.defense = 5;
+  }
+  public hurtAlien (Alien a) {
+    // todo
+  }
+}
